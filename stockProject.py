8bdce5a22bed1c8cecadafbf97024ac2b2cdec47
RoboBlawck <jjpwn1@gmail.com>
2018-03-09 20:19:24 -0800

    Initial commit

---
+++ stockProject.py
@@ -0,0 +1,17 @@
+import datetime
+import matplotlib.pyplot
+from matplotlib import style
+import pandas
+import pandas_datareader.data as web
+
+#Initilization started#
+style.use('ggplot')
+currentTime = datetime.datetime.now()
+#Only works for up to 5 years#
+source = 'iex'
+start = datetime.datetime(2017,1,1)
+end = datetime.datetime(currentTime.year,currentTime.month,currentTime.day)
+companyData_1 = web.DataReader(company, source, start, end)
+companyData_2 = web.DataReader(company, source, start, end)
+#Initilization ended#
+
